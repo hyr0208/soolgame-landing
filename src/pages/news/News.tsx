c98d3c1d1c5179cbda6a30e9711bb092e32ba496
yyyerin <hyr0208@naver.com>
2025-11-19 14:53:27 +0900

    feat: News 페이지 추가

---
+++ src/pages/news/News.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function News() {
+  return (
+    <div className="pt-24 min-h-screen bg-white">
+      <div className="max-w-7xl mx-auto px-4 py-8">
+        <h1 className="text-4xl font-bold mb-8 text-black">새소식</h1>
+        <div className="text-gray-700">News</div>
+      </div>
+    </div>
+  );
+}
+
+export default News;
